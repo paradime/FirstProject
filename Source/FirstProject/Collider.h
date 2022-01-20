fe1cc5bd8777d5eefd9e1924d97b31383c457896
Bryon Wilkins <bryonwilkins94@gmail.com>
2022-01-20 12:16:01 -0500

    readd files

---
+++ Source/FirstProject/Collider.h
@@ -0,0 +1,72 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "Collider.generated.h"
+
+UCLASS()
+class FIRSTPROJECT_API ACollider : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	ACollider();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UPROPERTY(VisibleAnywhere, Category = "Mesh")
+		class UStaticMeshComponent* MeshComponent;
+
+	UPROPERTY(VisibleAnywhere, Category = "Mesh")
+		class USphereComponent* SphereComponent;
+
+	FORCEINLINE UStaticMeshComponent* GetMeshComponent() {
+		return MeshComponent;
+	}
+	FORCEINLINE void SetMeshComponent(UStaticMeshComponent* mesh) {
+		MeshComponent = mesh;
+	}
+
+	FORCEINLINE USphereComponent* GetSphereComponent() {
+		return SphereComponent;
+	}
+	FORCEINLINE void SetSphereComponent(USphereComponent* sphere) {
+		SphereComponent = sphere;
+	}
+
+	UPROPERTY(VisibleAnywhere, Category = "Mesh")
+		class UCameraComponent* Camera;
+
+	FORCEINLINE UCameraComponent* GetCameraComponent() {
+		return Camera;
+	}
+	FORCEINLINE void SetCameraComponent(UCameraComponent* camera) {
+		Camera = camera;
+	}
+
+	UPROPERTY(VisibleAnywhere, Category = "Mesh")
+		class USpringArmComponent* SpringArm;
+	FORCEINLINE USpringArmComponent* GetSpringArmComponent() {
+		return SpringArm;
+	}
+	FORCEINLINE void SetSpringArmComponent(USpringArmComponent* springArm) {
+		SpringArm = springArm;
+	}
+
+private:
+	void MoveForward(float input);
+	void MoveRight(float input);
+
+};
